91a1b6aa79fc67424c019490b66a15466d9218d3
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-20 08:58:44 -0300

    Initial commit

---
+++ src/main/java/com/ariel/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ariel.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+}

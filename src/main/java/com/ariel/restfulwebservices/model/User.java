12fa5a57ea5815a473ba2a1e279c533f2158aa05
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-20 13:36:24 -0300

    Validação para User.

--- src/main/java/com/ariel/restfulwebservices/model/User.java
+++ src/main/java/com/ariel/restfulwebservices/model/User.java
@@ -2,9 +2,14 @@ package com.ariel.restfulwebservices.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 
@@ -15,7 +20,14 @@ public class User {
     @GeneratedValue
     private long id;
 
+    @Column(nullable = false)
+    @NotBlank
+    @Size(max = 255)
     private String name;
+
+    @Column(nullable = false)
+    @NotEmpty
+    @Past
     private Date birthDate;
 
     protected User() {

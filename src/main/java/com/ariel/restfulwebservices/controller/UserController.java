66a102bdcfded0982bfd86994444c642739cc017
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-01-21 10:34:02 -0300

    FIX — Levantando exceção quando usuário não estiver presente no BD.

--- src/main/java/com/ariel/restfulwebservices/controller/UserController.java
+++ src/main/java/com/ariel/restfulwebservices/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ariel.restfulwebservices.controller;
 
+import com.ariel.restfulwebservices.exception.UserNotFoundException;
 import com.ariel.restfulwebservices.model.Post;
 import com.ariel.restfulwebservices.model.User;
 import com.ariel.restfulwebservices.service.PostService;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -33,11 +35,16 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public EntityModel<User> getUserById(@PathVariable long id) {
-        User user = userService.getUserById(id);
-        EntityModel<User> model = EntityModel.of(user);
-        WebMvcLinkBuilder linkToUser = linkTo(methodOn(this.getClass()).getAllUsers());
-        model.add(linkToUser.withRel("all-users"));
-        return model;
+        Optional<User> user = userService.getUserById(id);
+
+        if (user.isEmpty()) {
+            throw new UserNotFoundException(id);
+        } else {
+            EntityModel<User> model = EntityModel.of(user.get());
+            WebMvcLinkBuilder linkToUser = linkTo(methodOn(this.getClass()).getAllUsers());
+            model.add(linkToUser.withRel("all-users"));
+            return model;
+        }
     }
 
     @PostMapping("/users")
@@ -51,7 +58,6 @@ public class UserController {
     public void deleteUserById(@PathVariable long id) {
         userService.deleteUserById(id);
     }
-
     ///////////
     // Posts //
     ///////////
